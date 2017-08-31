507668ee3651f3e2c4009105e40d1711ef76871c
thenam1906 <thangpqse04900@fpt.edu.vn>
2017-08-31 11:32:21 +0700

    commit changes

---
+++ app/src/main/java/com/example/linhphan/bai3version2/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.example.linhphan.bai3version2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Linh Phan on 8/31/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+}
